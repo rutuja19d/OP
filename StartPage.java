88594878a67ef7f6080dd690ba0654e5657b0c92
rutuja19d <54698142+rutuja19d@users.noreply.github.com>
2019-09-09 16:09:00 +0530

    Add files via upload

---
+++ StartPage.java
@@ -0,0 +1,62 @@
+package com.gladydennyson.dion.onpoint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class StartPage extends AppCompatActivity {
+
+    private String input_String;
+    String var="hello";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+    }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event){
+//        switch (keyCode){
+//            case KeyEvent.KEYCODE_DPAD_CENTER:
+//            case KeyEvent.KEYCODE_ENTER:
+//                makeToast("key pressed");
+//                return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+//
+//    private void makeToast(String message){
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//    }
+
+
+
+
+
+
+
+    private String displayInput() {
+        EditText editText = (EditText) findViewById(R.id.enterWord);
+        String sub = editText.getText().toString();
+        return sub;
+    }
+
+    public void startSearch(View view){
+
+        input_String = displayInput();
+        Intent intent = new Intent(StartPage.this,SearchPage.class);
+        intent.putExtra("key", input_String);
+        startActivity(intent);
+    }
+
+}
